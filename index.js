f9bbdf29530226b2753215ee176b4835ad8d9ac0
Humairaosama9298 <faryalfilza@gmail.com>
2024-04-29 09:29:41 +0500

    first commit
    Hello World in TypeScript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// print Hello world
+// Method 1:
+console.log("Hello world");
+// Method 2:
+let message = ("Hello World");
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,11 @@
+
+// print Hello world
+
+
+// Method 1:
+console.log("Hello world");
+
+
+// Method 2:
+let message:string = ("Hello World");
+console.log(message);
